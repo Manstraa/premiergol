4c2dbf001def11094978a62429facde5d02505b9
Manstraa <taylanozdemirciq7@gmail.com>
2022-02-21 16:29:25 +0300

    premiergol

---
+++ ggol_kralligii.py
@@ -0,0 +1,17 @@
+#adim 1 : fonksiyon olustur
+#adim 2  : girdilerini olustur
+#adim 3 : girfilerin ciktilari olsun if kullan
+
+
+
+def gol() :
+    krallik=input('Sira Numarasini Girin :')
+    if krallik==1 :
+        print('MO Salah - 17 gol -  Liverpool')
+    elif krallik==2 :
+        print('Diogo Jota - 12 gol - Liverpool')
+    if krallik==3 :
+        print('Raheem Sterling - 10 gol - Manchester City')
+    elif krallik==4 :
+        print('Cristiano Ronaldo - 9 gol - Manchester United')
+print(gol())
